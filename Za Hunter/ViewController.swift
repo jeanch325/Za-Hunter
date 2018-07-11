22ad2e544547cffe245f3901aed9c25a795bdde1
Jean Cho <jeancho@jeans-macbook-air.local>
2018-07-11 11:28:03 -0500

    zooms in on current location

--- Za Hunter/ViewController.swift
+++ Za Hunter/ViewController.swift
@@ -10,19 +10,32 @@ import UIKit
 import MapKit
 import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
 
     @IBOutlet weak var mapView: MKMapView!
     
     let locationManager = CLLocationManager()
+    var region = MKCoordinateRegion()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         locationManager.requestWhenInUseAuthorization()
         mapView.showsUserLocation = true
+        locationManager.delegate = self
+        locationManager.startUpdatingLocation()
         
     }
 
+    //zooms in on current location
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let location = locations.first!
+        let center = CLLocationCoordinate2DMake(location.coordinate.latitude, location.coordinate.longitude) //center
+        let span = MKCoordinateSpanMake(0.025, 0.025) //span
+        let region = MKCoordinateRegionMake(center, span) //the region is the distance of the span from the center
+        mapView.setRegion(region, animated: true)
+        
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
